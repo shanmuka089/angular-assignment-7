97caa7ea967c6e1d18e9ca919461605b63ce73f2
shanmukha <shanmukhashanu5525@gmail.com>
2024-06-06 15:17:38 +0530

    ssignment-7 completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,65 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    ReactiveFormsModule,
+    CommonModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
+  signupForm!: FormGroup;
+
+  mockProjectName = 'Test';
+  maockMail = 'tests@gmail.com';
+
+  ngOnInit(): void {
+    this.signupForm = new FormGroup({
+      'projectName' : new FormControl(null, [Validators.required, Validators.minLength(3), this.forbiddenProjectName.bind(this)]),
+      'mail' : new FormControl(
+        null, 
+        [Validators.required, Validators.email], 
+        [this.forbiddenEmail.bind(this)]),
+      'projectStatus' : new FormControl('Stable')
+    })
+  }
+
+  
+
+  projectStatuses = ['Stable', 'Critical', 'Finished']
+
   title = 'assignment-7';
+
+  onSubmit() {
+    console.log(this.signupForm);
+  }
+
+  forbiddenProjectName(control: FormControl): {[s:string]:boolean} | null{
+    if(this.mockProjectName === control.value) {
+      return {'forbiddenProjectName' : true};
+    }
+    return null;
+  }
+
+  forbiddenEmail(control: AbstractControl) : Promise<{[s:string]:boolean}> | Observable<{[s:string]:boolean}> {
+    const promise = new Promise<any>((resolve, reject) => {
+      setTimeout(()=> {
+        if(this.maockMail === control.value) {
+          resolve({'forbiddenMail': true});
+        } else {
+          resolve(null);
+        }
+      }, 1500);
+    })
+    return promise;
+  }
 }
